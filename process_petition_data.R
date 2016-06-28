166aad223322b69dabeaa081ca5e4b9772a400cd
Gregory Jefferis <jefferis@gmail.com>
2016-06-28 19:42:28 +0100

    use timezone
    * doesn't affect plots but hey it's Europe/London!

--- process_petition_data.R
+++ process_petition_data.R
@@ -6,6 +6,6 @@ library(httr)
 ll2=sapply(ll, function(x) fromJSON(content(x, type='text')), simplify = F)
 saveRDS(ll2, file='munged_petition_data.rds')
 sdf=data.frame(f=I(ff), 
-               time=lubridate::parse_date_time(names(ff), orders = "ymdhms"))
+               time=lubridate::parse_date_time(names(ff), orders = "ymdhms", tz="Europe/London"))
 sdf$total=sapply(ll2, function(x) x$data$attributes$signature_count)
 saveRDS(sdf, file='signature_data.rds')
